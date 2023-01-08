6ee2a7f56138fb6a981617c3f33468e4e908d864
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2023-01-08 20:30:11 +0200

    html configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,58 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: ['babel-polyfill', './src/js/controller.js'],
+  entry: ["babel-polyfill", "./src/js/controller.js"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
     // filename: 'bundle.js',
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, './dist'),
+      directory: path.join(__dirname, "./dist"),
     },
     // hot: true,
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebPlugin({
-      filename: 'index.html',
-      template: './index.html',
+      filename: "index.html",
+      template: "./index.html",
     }),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: 'style.css',
-      chunkFilename: '[name].css',
+      filename: "style.css",
+      chunkFilename: "[name].css",
     }),
   ],
   module: {
     rules: [
       {
-        test: '/.js$/',
-        exclude: '/node_modules/',
+        test: "/.js$/",
+        exclude: "/node_modules/",
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-class-properties'],
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
           },
         },
       },
       {
-        test: /\.(s(a|c)ss)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.html$/,
-        loader: 'html-loader',
+        loader: "html-loader",
       },
       {
         test: /\.(svg|gif)$/i,
-        use: ['file-loader'],
+        use: ["file-loader"],
       },
     ],
   },
